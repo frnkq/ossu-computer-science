4181e73277ddc9effdea013029406ec749864588
Franco Canevali <francocanevali@gmail.com>
2022-10-03 21:20:59 -0300

    Fingers

--- intro-to-cs/finger-25.py
+++ intro-to-cs/finger-25.py
@@ -14,6 +14,18 @@ def sum_string(str, current_pos = 0, number_string = '', sum = 0):
     current_pos += 1
     return sum_string(str, current_pos, number_string, sum)
 
-s = "1.23,2.4,3.123,8999992828982.555"
-sum = sum_string(s)
-print("Sum: ", sum)
+##Test
+strings_of_numbers_separated_by_comma = [
+    "123,44,6.5,2,3,5,6,888,9.8,9.0,-1,8288",
+    "123,44,6.5,2,3,58,5.3,9.98,6,888,9.8,9.0,-1,8288",
+    "12883,44,677.5,2,3,588888000,6,888,9.8,0,-1,8288",
+    "1,2,3",
+    "4,5,6",
+    "9,10,11",
+]
+arr = strings_of_numbers_separated_by_comma
+for i in range(len(arr)):
+    sum = 0
+    for j in range(0, len(arr[i].split(','))):
+        sum += float(arr[i].split(',')[j])
+    print(f"String: {arr[i]} \n Sum by Python: {sum}\n Sum by frnkq: {sum_string(arr[i])} \n ")
